5426aa45c61addf6d429b8dc2dccb52f2acce82a
Krzysztof Kiełczewski <kkielek97@gmail.com>
2019-04-01 14:53:04 +0200

    "Zabawa" z modyfikowaniem przykładowego wykresu

--- Biometrix/BasicColumn.xaml.cs
+++ Biometrix/BasicColumn.xaml.cs
@@ -19,21 +19,31 @@ namespace Biometrix
                 new ColumnSeries
                 {
                     Title = "2015",
-                    Values = new ChartValues<double> { 10, 50, 39, 50 }
+                    Values = new ChartValues<double>()
                 }
             };
 
             //adding series will update and animate the chart automatically
-            SeriesCollection.Add(new ColumnSeries
-            {
-                Title = "2016",
-                Values = new ChartValues<double> { 11, 56, 42 }
-            });
+            //SeriesCollection.Add(new ColumnSeries
+            //{
+            //    Title = "2016",
+            //    Values = new ChartValues<double> { 11, 56, 42 }
+            //});
 
             //also adding values updates and animates the chart automatically
-            SeriesCollection[1].Values.Add(48d);
+            SeriesCollection[0].Values.Add(10d);
+            SeriesCollection[0].Values.Add(50d);
+            SeriesCollection[0].Values.Add(39d);
+            SeriesCollection[0].Values.Add(50d);
+            SeriesCollection[0].Values.Add(129.49d);
+            SeriesCollection[0].Values.Add(15d);
+            SeriesCollection[0].Values.Add(15d);
 
-            Labels = new[] { "Maria", "Susan", "Charles", "Frida" };
+            Labels = new string[256];
+            for (int i = 0; i < 256; i++)
+            {
+                Labels[i] = i.ToString();
+            }
             Formatter = value => value.ToString("N");
 
             DataContext = this;

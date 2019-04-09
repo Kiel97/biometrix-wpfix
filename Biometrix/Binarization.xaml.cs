47f6f910bd84d045bef98a95a0f3f7ae8af71d5f
Krzysztof Kiełczewski <kkielek97@gmail.com>
2019-04-09 23:24:22 +0200

    Dodano źle działający otsu

--- Biometrix/Binarization.xaml.cs
+++ Biometrix/Binarization.xaml.cs
@@ -105,6 +105,118 @@ namespace Biometrix
             UpdatePreviewImage(p);
         }
 
+        private void OtsuThresholdCalculate()
+        {
+            int[] histogram = HistogramCreator.GetHistogramFromByteArray(pixels, HistogramCreator.ColorMode.GRAYSCALE);
+            int threshold = 0;
+
+            double minClassVariance = double.MaxValue;
+            int sumOfPixels = histogram.Sum();
+
+            for (int T = 0; T < 256; T++)
+            {
+                double weightBackground = 0, meanBackground = 0, varianceBackground = 0, weightForeground = 0, meanForeground = 0, varianceForeground = 0;
+                double withinClassVariance = double.MaxValue;
+                int pixelsFromZeroToThreshold = 0;
+                int pixelsFromThresholdTo255 = 0;
+
+                //weightBackground
+                for (int i = 0; i < T; i++)
+                {
+                    pixelsFromZeroToThreshold += histogram[i];
+                }
+                weightBackground = pixelsFromZeroToThreshold / sumOfPixels;
+
+                //meanBackground
+                int subSum = 0;
+                for (int i = 0; i < T; i++)
+                {
+                    subSum += (i * histogram[i]);
+                }
+
+                try
+                {
+                    meanBackground = subSum / pixelsFromZeroToThreshold;
+                }
+                catch (DivideByZeroException)
+                {
+                    meanBackground = subSum / 1;
+                }
+                
+
+                //varianceBackground
+                double subSumFloat = 0;
+                for (int i = 0; i < T; i++)
+                {
+                    subSumFloat += (Math.Pow(i - meanBackground, 2)) * histogram[i];
+                }
+
+                try
+                {
+                    varianceBackground = subSumFloat / pixelsFromZeroToThreshold;
+                }
+                catch (DivideByZeroException)
+                {
+                    varianceBackground = subSumFloat / 1;
+                }
+                
+
+                //---------------------------------------------------------------
+
+                //weightForeground
+                for (int i = T; i < 256; i++)
+                {
+                    pixelsFromThresholdTo255 += histogram[i];
+                }
+                weightForeground = pixelsFromThresholdTo255 / sumOfPixels;
+
+                //meanForeground
+                int subSumF = 0;
+                for (int i = T; i < 256; i++)
+                {
+                    subSumF += (i * histogram[i]);
+                }
+
+                try
+                {
+                    meanForeground = subSumF / pixelsFromThresholdTo255;
+                }
+                catch (DivideByZeroException)
+                {
+                    meanForeground = subSumF / 1;
+                }
+                
+
+                //varianceForeground
+                double subSumFloatF = 0;
+                for (int i = T; i < 256; i++)
+                {
+                    subSumFloatF += (Math.Pow(i - meanForeground, 2)) * histogram[i];
+                }
+
+                try
+                {
+                    varianceForeground = subSumFloatF / pixelsFromThresholdTo255;
+                }
+                catch (DivideByZeroException)
+                {
+                    varianceForeground = subSumFloatF / 1;
+                }
+                
+
+                withinClassVariance = weightBackground * varianceBackground + weightForeground * varianceForeground;
+                if (withinClassVariance < minClassVariance)
+                {
+                    threshold = T;
+                    minClassVariance = withinClassVariance;
+                }
+            }
+
+            MessageBox.Show($"Znaleziony optymalny próg metodą Otsu wynosi {threshold}.");
+
+            ThresholdSpinValue.Value = (byte)threshold;
+        }
+
         private void ConfirmButton_Click(object sender, RoutedEventArgs e)
         {
             this.DialogResult = true;
@@ -115,6 +227,11 @@ namespace Biometrix
         {
             this.DialogResult = false;
             this.Close();
+        }
+
+        private void OtsuThresholdBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OtsuThresholdCalculate();
         }
     }
 }

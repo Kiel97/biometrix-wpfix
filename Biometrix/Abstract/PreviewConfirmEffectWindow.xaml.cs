9089db172397adc9bb1275619841a6a2acbf0d40
Krzysztof Kiełczewski <kkielek97@gmail.com>
2019-05-07 23:51:20 +0200

    Wyłuskałem część wspólną okienek pod przyszłe okienka

---
+++ Biometrix/Abstract/PreviewConfirmEffectWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Biometrix.Abstract
+{
+    /// <summary>
+    /// Logika interakcji dla klasy PreviewConfirmEffectWindow.xaml
+    /// </summary>
+    public partial class PreviewConfirmEffectWindow : Window
+    {
+        public byte[] modifiedPixels;
+
+        WriteableBitmap previewBitmap;
+        byte[] pixels;
+        int stride;
+        int bytesPerPixel;
+        int width;
+        int height;
+
+        public PreviewConfirmEffectWindow(byte[] pixels, int stride, int width, int height, WriteableBitmap modifiedBitmap, int bytesPerPixel)
+        {
+            InitializeComponent();
+            previewBitmap = new WriteableBitmap(modifiedBitmap);
+
+            this.pixels = pixels;
+            this.stride = stride;
+            this.width = width;
+            this.height = height;
+            this.bytesPerPixel = bytesPerPixel;
+
+            modifiedPixels = new byte[pixels.Length];
+
+            UpdatePreviewImage(pixels);
+        }
+
+        private void UpdatePreviewImage(byte[] pixels)
+        {
+            BitmapSource bitmap = BitmapImage.Create(width, height, 96, 96, PixelFormats.Bgr32, null, pixels, stride);
+            previewBitmap = new WriteableBitmap(bitmap);
+
+            PreviewImage.Source = previewBitmap;
+
+            modifiedPixels = pixels;
+        }
+
+        private void PreviewButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Wywołanie głównej funkcji
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}
